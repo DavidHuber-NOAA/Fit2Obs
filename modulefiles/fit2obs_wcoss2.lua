2423e97dfd18e9313c199d9dcc6f1f1dfc75990b
David Huber <david.huber@noaa.gov>
2025-01-03 18:55:26 +0000

    Build with spack-stack on WCOSS2

--- modulefiles/fit2obs_wcoss2.lua
+++ modulefiles/fit2obs_wcoss2.lua
@@ -2,44 +2,18 @@ help([[
 Build environment for fit2obs on WCOSS2
 ]])
 
-local PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.1.0"
-local intel_ver=os.getenv("intel_ver") or "19.1.3.304"
-local craype_ver=os.getenv("craype_ver") or "2.7.10"
-local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.9"
-local cmake_ver= os.getenv("cmake_ver") or "3.20.2"
+prepend_path("MODULEPATH", "/apps/ops/test/spack-stack-1.6.0-nco/envs/nco-intel-19.1.3.304/install/modulefiles/Core")
 
-local jasper_ver=os.getenv("jasper_ver") or "2.0.25"
-local zlib_ver=os.getenv("zlib_ver") or "1.2.11"
-local libpng_ver=os.getenv("libpng_ver") or "1.6.37"
+local stack_python_ver=os.getenv("python_ver") or "3.10.13"
+local stack_intel_ver=os.getenv("stack_intel_ver") or "19.1.3.304"
+local stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.9"
+local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 
-local netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
+load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 
-local bufr_ver=os.getenv("bufr_ver") or "11.7.0"
-local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
-local w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
-local sp_ver=os.getenv("sp_ver") or "2.3.3"
-local sigio_ver=os.getenv("sigio_ver") or "2.3.2"
-local sfcio_ver=os.getenv("sfcio_ver") or "1.4.1"
-local nemsio_ver=os.getenv("nemsio_ver") or "2.5.2"
+pushenv("bufr_ver", "12.0.1")
 
-load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
-load(pathJoin("intel", intel_ver))
-load(pathJoin("craype", craype_ver))
-load(pathJoin("cray-mpich", cray_mpich_ver))
-load(pathJoin("cmake", cmake_ver))
-
-load(pathJoin("jasper", jasper_ver))
-load(pathJoin("zlib", zlib_ver))
-load(pathJoin("libpng", libpng_ver))
-
-load(pathJoin("netcdf", netcdf_ver))
-
-load(pathJoin("bufr", bufr_ver))
-load(pathJoin("bacio", bacio_ver))
-load(pathJoin("w3emc", w3emc_ver))
-load(pathJoin("sp", sp_ver))
-load(pathJoin("sigio", sigio_ver))
-load(pathJoin("sfcio", sfcio_ver))
-load(pathJoin("nemsio", nemsio_ver))
+load("fit2obs_common")
 
 whatis("Description: fit2obs environment on WCOSS2 with Intel Compilers")
